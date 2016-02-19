583ff043420125efd6cd72a1ad77525b18f11c1f
Arnau Marti <armarti13@gmail.com>
2016-02-19 23:09:48 +0100

    Initial Commit

---
+++ Feed/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Feed
+//
+//  Created by Arnau Marti Font on 19/2/16.
+//  Copyright © 2016 Arnau Marti Font. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
